e1553120099475a7b6942ae94a4504c7c52ddb3d
delvauxo <delvauxo@outlook.com>
2022-02-06 03:48:14 +0100

    includes function path fixed

--- public/js/includes.js
+++ public/js/includes.js
@@ -1,7 +1,7 @@
 const includeHTMLs = async function(arrayIncludesWrappers) {
   for (const include of arrayIncludesWrappers) {
       const page = include.getAttribute('include-html')
-      const response = await fetch('./public/pages/' + page + '.html', {method: 'GET'})
+      const response = await fetch('./pages/' + page + '.html', {method: 'GET'})
       const text = await response.text()
       include.innerHTML = text
   }
